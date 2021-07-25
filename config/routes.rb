9398080ba3b706be65ea8a88f5b920acfbc18d98
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-07-25 17:17:11 +0000

    [ADD]管理者側のルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  namespace :admin do
+    root 'homes#top'
+
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_items, only: [:update]
+  end
+
 end
