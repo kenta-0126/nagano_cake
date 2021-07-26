a2b76980251f304e220076181d1dc58bde8ec1cc
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-07-26 11:59:05 +0000

    [ADD]controllerにアクションを指定

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,18 @@
 class CartItemsController < ApplicationController
+  
+  def index
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def empty
+  end
+  
+  def create
+  end
+  
 end
