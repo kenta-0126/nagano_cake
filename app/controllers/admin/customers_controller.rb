e14a63b8d3e77ed15342f0a2b26ba2a705e74755
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-08-01 18:33:35 +0000

    [ADD]マイページの作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CustomersController < ApplicationController
   
   def index
+    
   end
   
   def show
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,18 +1,19 @@
 class CustomersController < ApplicationController
-  
+
   def show
+    @customer = current_customer
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def confirm
   end
-  
+
   def withdraw
   end
-  
+
 end
