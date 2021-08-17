57f38943bc28917372a2e181fcd192b770cb87b0
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-08-17 21:41:03 +0000

    [ADD]itemモデルにvalidation追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,9 @@ class Item < ApplicationRecord
 
     enum is_active: { on_sale: 0, off_sale: 1 }
 
-    def self.search(search)
-        if search
-            where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。#User.は省略
-        else
-            all #全て表示。#User.は省略
-        end
-    end
+    
 
+  validates :name, {presence: true}
+  validates :introduction, {presence: true}
+  validates :price, {presence: true}
 end
