d0f177f0c8d25fb4beb076069736677c4bf9f8c3
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-08-18 05:55:18 +0000

    [ADD]会員詳細ページのレイアウト

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   
   enum payment_method: { credit: 0, bunk: 1 }
-  enum status: { a: 0, b: 1, c: 2, d: 3, e: 4 }
+  enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
 end
