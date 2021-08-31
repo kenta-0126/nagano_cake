42fe3b8b32269bd5bcb3b8d3b309e7ffd0935670
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-08-31 04:03:09 +0000

    [ADD]itemのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
     
 
-  validates :name, {presence: true}
-  validates :introduction, {presence: true}
-  validates :price, {presence: true}
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
 end
