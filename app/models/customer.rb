03745a1c5943d2ca395fb71d2360463f65fa17bb
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-08-31 04:00:56 +0000

    [ADD]genreのバリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,6 +9,9 @@ class Customer < ApplicationRecord
   has_many :orders
   has_many :addresses
   
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence: true
+  
+  
   def full_name
     first_name + last_name
   end
