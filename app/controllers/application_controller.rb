0cfb3a145fc5294f08a7b2abc8a5b90c2f98b4f5
EC2 Default User <ec2-user@ip-172-31-78-187.ec2.internal>
2021-09-01 13:30:58 +0000

    [ADDcustomer側のitem.indexページのレイアウト]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 class ApplicationController < ActionController::Base
  before_action :authenticate_customer!,except: [:top, :about]
- before_action :authenticate_admin!,except: [:top, :about]
+ 
  
  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
    def after_sign_in_path_for(resource)
-        case resource
-        when Admin
-            admin_root_path
-        when Customer
-            customers_my_page_path
-        end
+      if current_customer
+         customers_my_page_path
+      else Admin
+          #　指定したいパスに変更
+      end
    end
 
 
